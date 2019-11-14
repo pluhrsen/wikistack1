84afb8282ad1e9b5af5bf49e3201f614d8786663
paulluhrsen <paulluhrsen@gmail.com>
2019-11-14 16:01:55 -0600

    markdown working

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const { userList, userPages } = require("../views");
+const { Page, User } = require ("../models");
 
-router.get("/", (req, res) => {
-  res.send('this is a user page');
+router.get("/", async (req, res) => {
+  const userArr = await User.findAll()
+  res.send(userList(userArr));
+})
+
+router.get("/:id", async (req, res, next) => {
+  try{
+  const foundUser = await User.findOne({
+    where: {id: req.params.id}
+  })
+  const foundPages = await Page.findAll({
+    where: { authorId: foundUser.id}
+  })
+  res.send(userPages(foundUser, foundPages));
+} catch (err) {
+  next(err)
+}
 })
 
 module.exports = router;
