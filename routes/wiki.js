e86cc884ac516e52699aa05012cbed8df029e5db
paulluhrsen <paulluhrsen@gmail.com>
2019-11-14 17:01:36 -0600

    edit partially done

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Page, User } = require ("../models");
-const { addPage, main, wikiPage } = require("../views");
+const { addPage, main, wikiPage, editPage } = require("../views");
 
 router.get("/", async (req, res, next) => {
   try {
@@ -46,11 +46,49 @@ router.get('/:slug', async (req, res, next) => {
     const foundPage = await Page.findOne({
       where: {slug: req.params.slug}
     })
-    const foundUser = await User.findOne({
-      where: {id: foundPage.authorId}
-    })
-    res.send(wikiPage(foundPage, foundUser))
+    if(!foundPage){
+      res.status(404).send("404 Page Not Found");
+    } else{
+      const foundUser = await User.findOne({
+        where: {id: foundPage.authorId}
+      })
+      res.send(wikiPage(foundPage, foundUser))
+    }
   } catch(err) {next(err)}
+})
+
+router.get("/:slug/edit", async (req, res, next) => {
+  try{
+  const foundPage = await Page.findOne({
+    where: {slug: req.params.slug}
+  })
+  const foundUser = await User.findOne({
+    where: {id: foundPage.authorId}
+  })
+  res.send(editPage(foundPage, foundUser))
+} catch(err) {next (err)}
+})
+
+router.post("/:slug", async (req, res, next) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const title = req.body.title;
+  const content = req.body.content;
+  const status = req.body.status;
+
+  const user = await User.findOrCreate({where: { name, email }})
+
+  const page = Page.update({
+    title,
+    content,
+    status
+  });
+
+  try{
+    await page.save();
+    await user[0].save();
+    res.redirect(`/wiki/${page.slug}`);
+  } catch (error) { next(error) }
 })
 
 module.exports = router;
--- views/editPage.js
+++ views/editPage.js
@@ -6,9 +6,19 @@ module.exports = (page, author) => layout(html`
   <hr>
   <form method="POST" action="/wiki/${page.slug}">
 
-    <div>PLACEHOLDER FOR AUTHOR NAME FIELD</div>
-    
-    <div>PLACEHOLDER FOR AUTHOR EMAIL FIELD</div>
+  <div>
+    <label class="col-sm-2 control-label">Author Name</label>
+    <div class="col-sm-10">
+     <input id="name" name="name" type="text" class="form-control" value="${author.name}"/>
+    </div>
+  </div>
+
+  <div>
+    <label class="col-sm-2 control-label">Author Email</label>
+    <div class="col-sm-10">
+      <input id="email" name="email" type="email" class="form-control" value="${author.email}"/>
+    </div>
+  </div>
 
     <div class="form-group">
       <label for="title" class="col-sm-2 control-label">Page Title</label>
@@ -17,7 +27,12 @@ module.exports = (page, author) => layout(html`
       </div>
     </div>
 
-    <div>PLACEHOLDER FOR PAGE CONTENT TEXTAREA FIELD</div>
+    <div>
+      <label class="col-sm-2 control-label">Content</label>
+      <div class="col-sm-10">
+        <input id="content" name="content" type="textarea" class="form-control" value="${page.content}"/>
+      </div>
+    </div>
 
     <div class="form-group">
       <label for="content" class="col-sm-2 control-label">Status</label>
